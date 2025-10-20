5a2a9e78000be0476c215567c6d95e178231431a
Vitaliy Potapov <vitaliy.potapov@takeda.com>
2025-10-20 13:50:09 +0200

    remove auth

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,4 +1,4 @@
-import {createAuthStore, defineConfig} from 'sanity'
+import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {internationalizedArray} from 'sanity-plugin-internationalized-array'
@@ -26,17 +26,4 @@ export default defineConfig({
       fieldTypes: ['string'],
     }),
   ],
-  auth: createAuthStore({
-    projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
-    dataset: process.env.SANITY_STUDIO_DATASET!,
-    mode: 'replace',
-    redirectOnSingle: false,
-    providers: [
-      {
-        name: 'saml',
-        title: 'saml',
-        url: process.env.SANITY_STUDIO_AUTH_URL!,
-      },
-    ],
-  }),
 })
